a1e15f5f9bac0ab7c03e732753f264e490295474
Sunny Kharel <53832042+sunnykharel@users.noreply.github.com>
2022-10-09 22:24:57 -0500

    Time: 59 ms (35.54%), Space: 13.9 MB (20.05%) - LeetHub

---
+++ 50-powx-n/50-powx-n.py
@@ -0,0 +1,25 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+        return x**n
+        
+        if(x==-1):
+            if n%2==1:
+                return -1
+            else:
+                return 1
+        elif n==0 or x==1.0:
+            return 1  
+        elif(n<0):
+            res=1.0
+            while(n<0 and 0<abs(res)<2147483648):
+                res/=x
+                n+=1
+            return res
+        else:
+            res=1.0
+            while(n>0 and 0<abs(res)<2147483648):
+                res*=x
+                n-=1
+            return res
+        

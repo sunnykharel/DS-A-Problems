23fdf9f366f20d815bd1acf9ad113cff236da9fa
Sunny Kharel <53832042+sunnykharel@users.noreply.github.com>
2022-10-09 22:23:06 -0500

    Time: 74 ms (15.82%), Space: 14.1 MB (35.72%) - LeetHub

---
+++ 78-subsets/78-subsets.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        subsetList = []
+        def subsets(depth, numbers):
+            if depth == n:
+                subsetList.append(list(numbers))
+            elif depth<n:
+                numbers.append(nums[depth])
+                subsets(depth+1, numbers)
+                numbers.pop()
+                subsets(depth+1, numbers)
+        subsets(0, [])
+        return subsetList
+            
+        

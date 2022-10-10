2b0f75a8b44db2cbf6537c0a1d0957db97af1c67
Sunny Kharel <53832042+sunnykharel@users.noreply.github.com>
2022-10-09 22:22:55 -0500

    Time: 110 ms (71.29%), Space: 14.1 MB (55.89%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,29 +1,21 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        # candidate numbers --> set
-        # target 
-        # find all unique combos in target that sum to target
-        # Recursive solution
-        # at every step, we can either choose or not choose
-        '''
-        at idx i: we can stay here, move forward, that is for every step
-        want to be able to stay here as long as curr_val+i*cand[i] < target 
-        '''
-        sol = []
-        def recursion(i, remaining, path):
-            if remaining < 0:
-                return
-            if remaining == 0:
-                if path not in sol:
-                    sol.append(path)
-            if i >=len(candidates):
-                return
-            else:
-                j = 1
-                while remaining-candidates[i]*j >= 0:
-                    recursion(i+1, remaining - candidates[i]*j, path+[candidates[i]]*j)
-                    j+=1
-                recursion(i+1, remaining, path)
-            return
-        recursion(0, target, [])
-        return sol
+        #To do problem using backtracking:
+        #basically do a bfs
+        candidates.sort()
+        targetSums = []
+        n = len(candidates)
+        def combinationSum(numbers, total, index):
+            if total == target:
+                targetSums.append(list(numbers))
+            elif total < target:
+                while index < n and candidates[index]+total <= target :
+                    num = candidates[index]
+                    numbers.append(num)
+                    combinationSum(numbers, total+num, index)
+                    numbers.pop()
+                    index+=1
+        combinationSum([], 0, 0)
+        return targetSums
+                
+        

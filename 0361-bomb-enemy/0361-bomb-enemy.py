2bf20896ddafa7f6b5c4d48169baa1f23805d1fe
Sunny Kharel <53832042+sunnykharel@users.noreply.github.com>
2022-11-15 09:53:21 -0600

    Time: 1430 ms (70.39%), Space: 52.6 MB (7.82%) - LeetHub

--- 0361-bomb-enemy/0361-bomb-enemy.py
+++ 0361-bomb-enemy/0361-bomb-enemy.py
@@ -4,8 +4,7 @@ class Solution:
         cols = len(grid[0])
         up, right, down, left = 0, 1, 2, 3
         memo = [[[0,0,0,0] for _ in range(cols)] for _ in range(rows)]
-        for row in memo:
-            print(row)
+
         def set_up_value(row, col):
             if grid[row][col] == 'W':
                 memo[row][col][up] = 0
@@ -17,7 +16,6 @@ class Solution:
                     int(grid[row][col] == 'E') + memo[row-1][col][up]
 
         def set_down_value(row, col):
-            print(row,col)
             if grid[row][col] == 'W':
                 memo[row][col][down] = 0
             elif row == rows-1:
@@ -46,7 +44,7 @@ class Solution:
             else:
                 memo[row][col][left] = \
                     int(grid[row][col] == 'E') + memo[row][col-1][left] 
-        
+
         for row in range(0,rows):
             for col in range(0,cols):
                 #get ups and lefts
@@ -80,66 +78,9 @@ class Solution:
                     coord_max_hits += memo[row][col][left]
                 opts[row][col] = coord_max_hits
                 max_hits = max(coord_max_hits, max_hits)
-        # you can only place the bomb in the empty cell
-        for row in grid:
-            print(row)
-        for row in memo:
-            print(row)    
-        print()
-        for row in opts:
-            print(row)
             
         expected = [
             [0,1,1,0], [1,1,2,1], [0,0,0,1], [0, 0, 1, 1]
             
         ]
         return max_hits
-    
-    
-                
-                
-            
-        '''
-        brute force:
-            you go to every index and then go left, right,
-            up, down until you hit a wall in each direction
-            this is a simple iteration in each direction.
-            
-            there is a property for each direction
-            opt needs to take direction as another source of info
-            
-            how do you populate the memoization?
-            if you are going down right, then you will 
-            get the up memos and the left memos
-            
-            if you are going up left then you will get
-            the right memos and the down memos
-            
-            memo = [[0 for _ in range()]]
-            
-            
-            opt(i,j, up) = (
-                0 if grid[i,j] == W,
-                1 if grid[i,j] == E else 0, i == 0,
-                1 if grid[i,j] == E else 0
-                    + opt(i,j-1, up)
-            )
-            opt(i,j, down) = (
-                0 if grid[i,j] == W,
-                1 if grid[i,j] == E else 0, i == n-1,
-                1 if grid[i,j] == E else 0
-                    + opt(i,j-1, down)
-            )
-            opt(i,j, down)
-            opt(i,j, left)
-            opt(i,j, right)
-            
-            opt(i,j) = (
-                1 if grid[i,j] == E else 0 + 
-                opt(i-1)
-            )
-            
-            opt(i,j,up) = 
-            
-        
-        '''

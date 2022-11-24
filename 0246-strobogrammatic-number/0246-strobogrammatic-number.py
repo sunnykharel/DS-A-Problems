e17c5f2067302f3cdc04518dbf18912d203f12b5
Sunny Kharel <53832042+sunnykharel@users.noreply.github.com>
2022-11-24 10:59:08 -0600

    Time: 46 ms (69.94%), Space: 13.8 MB (97.06%) - LeetHub

--- 0246-strobogrammatic-number/0246-strobogrammatic-number.py
+++ 0246-strobogrammatic-number/0246-strobogrammatic-number.py
@@ -1,12 +1,9 @@
 class Solution:
     def isStrobogrammatic(self, num: str) -> bool:
-        non_strobo = set(['2', '3', '4', '5', '7'])
+        non_strobo = {'2', '3', '4', '5', '7'}
         strobo_pairs = {
             '0':'0', '1':'1', '6':'9', '8':'8', '9':'6',
         }
-        # 5//2 = 0, 1, 2 : it goes till 1. we want it to go to 2
-        # 1//2 = 0
-        # 0//2 = 0
         n = len(num)
         for i in range(n//2 + 1):
             l, r = num[i], num[-(i+1)]

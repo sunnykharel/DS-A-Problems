f5696088c45e3c69f8314d52f32eca9c798dd05d
Sunny Kharel <53832042+sunnykharel@users.noreply.github.com>
2022-11-24 11:28:12 -0600

    Time: 643 ms (18.97%), Space: 25.5 MB (95.45%) - LeetHub

--- 0247-strobogrammatic-number-ii/0247-strobogrammatic-number-ii.py
+++ 0247-strobogrammatic-number-ii/0247-strobogrammatic-number-ii.py
@@ -4,18 +4,15 @@ class Solution:
         self.strobo_pairs = {
             '0':'0', '1':'1', '6':'9', '8':'8', '9':'6',
         }
+        self.non_strobo = {'2', '3', '4', '5', '7'}
 
     def is_strobogrammatic(self, num: list) -> bool:
-        non_strobo = {'2', '3', '4', '5', '7'}
-        strobo_pairs = {
-            '0':'0', '1':'1', '6':'9', '8':'8', '9':'6',
-        }
         n = len(num)
         for i in range(n//2 + 1):
             l, r = num[i], num[-(i+1)]
-            if l in non_strobo or r in non_strobo:
+            if l in self.non_strobo or r in self.non_strobo:
                 return False
-            if strobo_pairs[l] != r or strobo_pairs[r] != l:
+            if self.strobo_pairs[l] != r or self.strobo_pairs[r] != l:
                 return False
         return True
     

fead829d8d4124ceeb3386911d1d1a84e397476e
Sunny Kharel <53832042+sunnykharel@users.noreply.github.com>
2022-11-24 14:55:23 -0600

    Time: 202 ms (24.52%), Space: 14.3 MB (44.74%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
@@ -1,13 +1,5 @@
 class Solution:
     def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
-        # we have the formula
-        # row * column -> sum -> sum goes into res[row][column]
-        # we can iterate on rows of the first matrix
-        #   then iterate on each of the cols of the second
-        
-        # brute force:
-        # for loop on rows for mat1 nested loop for cols on mat2
-        
         rows = len(mat1)
         cols = len(mat2[0])
         result = [[0 for _ in range(cols)] for _ in range(rows)]

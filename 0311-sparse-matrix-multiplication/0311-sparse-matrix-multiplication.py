7a5a8b98f86ca80e2e822472bcc118663c117764
Sunny Kharel <53832042+sunnykharel@users.noreply.github.com>
2022-11-24 15:03:26 -0600

    Time: 206 ms (24.17%), Space: 14.4 MB (13.02%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
@@ -6,8 +6,7 @@ class Solution:
         
         for r in range(rows):
             for c in range(cols):
-                for k in range(len(mat2)):
-                    result[r][c] += mat1[r][k]*mat2[k][c]
+                result[r][c] += sum(mat1[r][k]*mat2[k][c] for k in range(len(mat2)))
                 # row = mat1[r]
                 # col = [mat2[k][c] for k in range(len(mat2))]
                 # value = sum(rc_vals[0]*rc_vals[1] for rc_vals in zip(row, col))
